ae15797ec3b95593724ceeb7f298d7f099856cf2
Johan Aldair <johan.ortiz.sist.922579406@gmail.com>
2025-12-19 17:05:16 -0500

    Conectar backend a MySQL Railway

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'sanilab_checklist',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 48583,
   multipleStatements: true
 });
 
