087e58fd1d2d211e2a02f35e93f6174a0bc89dea
Johan Aldair <johan.ortiz.sist.922579406@gmail.com>
2025-12-23 15:01:47 -0500

    Cambios en Database

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-  multipleStatements: true
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
