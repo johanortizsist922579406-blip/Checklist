f381c836861eff1b2c67a224850f0548ba96e6f7
Johan Aldair <johan.ortiz.sist.922579406@gmail.com>
2025-12-23 14:16:28 -0500

    Cambios en Database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'sanilab_checklist',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'sanilab_checklist',
   multipleStatements: true
 });
 
